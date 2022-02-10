ffe943d978d1afd782a98801480f563a98923d3d
fcmdhyd <fcmdhyd@gmail.com>
2022-02-10 22:43:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def move_counterclockwise():
+    tim.left(10)
+
+
+def move_clockwise():
+    tim.right(10)
+
+
+def move_clear():
+    tim.home()
+    tim.clear()
+
+
+screen.listen()
+screen.onkey(key="w",fun=move_forwards)
+screen.onkey(key="s",fun=move_backwards)
+screen.onkey(key="a",fun=move_counterclockwise)
+screen.onkey(key="d",fun=move_clockwise)
+screen.onkey(key="c",fun=move_clear)
+screen.exitonclick()
